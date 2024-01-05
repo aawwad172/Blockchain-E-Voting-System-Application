68b92cdb94a0f6aeb3b7ec5b7d9b8c304b3476a3
Ahmad Awwad <aawwad172@gmail.com>
2024-01-06 02:37:29 +0300

    Finishing Student Class

--- Classes/Student.cs
+++ Classes/Student.cs
@@ -6,5 +6,40 @@ using System.Threading.Tasks;
 
 namespace Blockchain_E_Voting_System_Application.Classes {
 	internal class Student {
+		private int studentID;
+		private string name;
+		private float gpa;
+		private int creditHours;
+		private String studentMajor;
+
+		// Property for studentID
+		public int StudentID {
+			get { return studentID; }
+			set { studentID = value; }
+		}
+
+		// Property for name
+		public string Name {
+			get { return name; }
+			set { name = value; }
+		}
+
+		// Property for gpa
+		public float GPA {
+			get { return gpa; }
+			set { gpa = value; }
+		}
+
+		// Property for creditHours
+		public int CreditHours {
+			get { return creditHours; }
+			set { creditHours = value; }
+		}
+
+		// Property for studentMajor
+		public String StudentMajor {
+			get { return studentMajor; }
+			set { studentMajor = value; }
+		}
 	}
 }
